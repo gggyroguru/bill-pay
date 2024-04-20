17d83606dc423535715f003c6741426623b32652
KUNAL KISHOR YADAV <141425134+gggyroguru@users.noreply.github.com>
2024-04-20 18:51:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 const input = document.querySelector('.input')
-const button = document.querySelector('.button')
+const calculate = document.querySelector('#calculate')
 const output = document.querySelector('.output')
+const clear = document.querySelector('#clear')
 
-button.addEventListener('click', () => {
-    const num = input.value.split('\n').map(i => i.split(' ')).flat()
-    const calculate = num.filter(i=> {
-        try {
-           return Number(i)
-        }
-        catch (e) {
-            
-        }
-    }).reduce((i, s) => Number(i) + Number(s))
-    console.log(calculate)
-    output.innerHTML = `total bill is : ${calculate}`
+calculate.addEventListener('click', () => {
+    if (input.value === '') {
+        output.innerHTML = `input bill`
+    } else {
+
+        const num = input.value.split('\n').map(i => i.split(' ')).flat()
+        const total = num.filter(i => {
+            try {
+                return Number(i)
+            } catch (e) {
+
+            }
+        }).reduce((i, s) => Number(i) + Number(s))
+        output.innerHTML = `total bill is : ${total}`
+    }
+})
+
+clear.addEventListener('click', () => {
+    input.value = ''
 })
